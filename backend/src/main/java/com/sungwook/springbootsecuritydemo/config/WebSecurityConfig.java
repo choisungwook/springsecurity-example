53d59a45443f11b7e2d1fe531c2995936cd0d059
choisungwook <kgg1959@naver.com>
2021-08-19 02:22:03 +0900

    feat: backend - helloworld 접근 머두 허용 #3

---
+++ backend/src/main/java/com/sungwook/springbootsecuritydemo/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.sungwook.springbootsecuritydemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                // "/"api 요청은 모두 허용하고 나머지는 deny
+                .authorizeRequests()
+                    .antMatchers("/").permitAll()
+                    .and()
+                // 로그인 페이지는 모두 허용
+                .formLogin()
+                    .permitAll()
+                    .and()
+                //  로그아웃 페이지는 모두 허용
+                .logout()
+                    .permitAll();
+    }
+}

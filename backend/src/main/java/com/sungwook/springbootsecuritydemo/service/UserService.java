a3653cf7de8ffe3e74e3bafc319e96fbd3736225
choisungwook <kgg1959@naver.com>
2021-09-11 14:00:22 +0900

    feat: 회원가입 중복가입 검사로직 추가

--- backend/src/main/java/com/sungwook/springbootsecuritydemo/service/UserService.java
+++ backend/src/main/java/com/sungwook/springbootsecuritydemo/service/UserService.java
@@ -4,12 +4,16 @@ import com.sungwook.springbootsecuritydemo.domain.Account;
 import com.sungwook.springbootsecuritydemo.domain.AccountRole;
 import com.sungwook.springbootsecuritydemo.domain.dto.CreateAccountRequestDto;
 import com.sungwook.springbootsecuritydemo.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
+@Slf4j
 public class UserService {
 
     @Autowired
@@ -21,6 +25,14 @@ public class UserService {
     @Transactional
     public Long CreateUser(CreateAccountRequestDto request_body){
         // default: 일방사용자 role적용
+
+        Optional<Account> exist_user = userRepository.findByUsername(request_body.getUsername());
+        // 가입된 사용자가 있으면 예외
+        if(exist_user.isPresent()) {
+            log.debug("회원가입 - 이미 존재하는 회원이어서 가입 실패 ->" + request_body.getUsername());
+            throw new IllegalStateException("이미 가입된 사용자입니다");
+        }
+
         Account new_user = Account.builder()
                 .username(request_body.getUsername())
                 .email(request_body.getEmail())
@@ -30,4 +42,5 @@ public class UserService {
 
         return userRepository.save(new_user).getId();
     }
+
 }
